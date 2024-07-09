d3b5f93501659bb8becf107b7445d5cb4b9b73d1
juanvelez <98352815+juanvelez9824@users.noreply.github.com>
2024-07-09 15:48:17 -0500

    sistema de monedas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
-{
+{   public int totalCoins = 0;
     public static GameManager Instance;
 
     // Start is called before the first frame update
@@ -21,4 +21,16 @@ public class GameManager : MonoBehaviour
     public void StartGame() {
         SceneManager.LoadScene(1);
     }
+
+    public void AddCoin(int value)
+    {
+        totalCoins += value;
+        Debug.Log($"Total de monedas : {totalCoins}");
+
+    }
+
+
+
+
+
 }
